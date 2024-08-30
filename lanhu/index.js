b9d5ac889b8f43a05ce4fd0844f1f671a6a9b7df
lvzegeng <1318086894@qq.com>
2024-08-30 17:36:20 +0800

    feat: 蓝湖替换CSS变量

--- lanhu/index.js
+++ lanhu/index.js
@@ -26,7 +26,7 @@ $borderRadius: 20px;
   // 复制代码按钮元素
   const copyBtnSelector = "#copy_code";
 
-  // 将 variable 转换为 [['#FA5944', '$colorPrimary']] 这样的结构。如果要修改替换值只需要修改这里的 '$colorPrimary'
+  // 将 variable 转换为 [['#FA5944', '$colorPrimary']] 这样的结构。如果要修改替换值只需要修改这里的 '$colorPrimary'。不区分 '#FA5944' 值的大小写进行替换
   const calcVariableArray = (variable) =>
     variable
       .split(";")
@@ -54,7 +54,7 @@ $borderRadius: 20px;
       const variableArray = calcVariableArray(item.variable);
 
       variableArray.forEach((i) => {
-        clipText = clipText.replaceAll(i[0], i[1]);
+        clipText = clipText.replaceAll(new RegExp(i[0], 'ig'), i[1]);
       });
 
       navigator.clipboard.writeText(clipText);
