fd82a512f0bfccf6cc0062e25ed094ceb3231501
lvzegeng <1318086894@qq.com>
2024-09-02 09:27:46 +0800

    feat: 蓝湖替换CSS变量

--- lanhu/index.js
+++ lanhu/index.js
@@ -2,7 +2,7 @@
 // @name         蓝湖替换CSS变量
 // @namespace    http://tampermonkey.net/
 // @version      0.0.3
-// @description  支持 Css、Less、Sass 变量；不区分大小写
+// @description  支持 Css、Less、Sass 变量；不区分大小写；多个相同值的变量会以注释替换在后面
 // @author       LZG
 // @match        https://lanhuapp.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
@@ -45,36 +45,52 @@ $borderRadius: 20px; /*注释*/
       document.querySelector(copyBtnSelector).click();
 
       let clipText = await navigator.clipboard.readText();
-      const variableArray = calcVariableArray(item.variable);
+      const variableObject = calcVariableObject(item.variable);
 
-      variableArray.forEach((i) => {
-        clipText = clipText.replaceAll(new RegExp(i[0], "ig"), i[1]);
+      Object.entries(variableObject).forEach(([key, value]) => {
+        clipText = clipText.replaceAll(new RegExp(key, "ig"), value);
       });
 
       navigator.clipboard.writeText(clipText);
     };
 
-    // 将 variable 转换为 [['#FA5944', '$colorPrimary'], ['#FA5944', 'calc(--colorPrimary)']] 这样的结构
-    const calcVariableArray = (variable) => {
+    // 将 variable 转换为这样的结构
+    // {
+    //     "#fa5944": "$colorPrimary /* $colorPrimary2, $colorPrimary23 */",
+    //     "#fa5944": "calc(--colorPrimary) /* calc(--colorPrimary2), calc(--colorPrimary23) */"
+    //     "20px": "$borderRadius"
+    // }
+    const calcVariableObject = (variable) => {
       // 使用正则表达式去掉注释
       const cleanedCode = variable
         .replace(/\/\/.*|\/\*[\s\S]*?\*\//g, "")
         .trim();
 
-      return cleanedCode
+      const tempResult = {}
+
+      cleanedCode
         .split(";")
         .slice(0, -1)
         .map((item) => item.split(":"))
-        .map((item) => {
-          const firstValue = item[1].trim();
+        .forEach((item) => {
+          const firstValue = item[1].trim().toLowerCase();
           let secondValue = item[0].trim();
 
           if (secondValue.startsWith("--")) {
             secondValue = `calc(${secondValue})`;
           }
 
-          return [firstValue, secondValue];
+          tempResult[firstValue] ??= []
+
+          tempResult[firstValue].push(secondValue)
         });
+
+      const result = {}
+      Object.entries(tempResult).map(([key, value])=> {
+        result[key] = value.length === 1? value[0]: `${value[0]} /* ${value.slice(1).join(', ')} */`
+      })
+
+      return result
     };
   })();
 })();
